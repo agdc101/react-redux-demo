fd2eda5db61eb2bf0f4df49f00db4f9611cd892c
agdc101 <agdc101@gmail.com>
2023-08-22 19:34:58 +0100

    new counter added, store changed

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,26 @@
 
 import { createStore } from 'redux';
 
-const reducer = (state = { counter: 0 }, action) => {
+const reducer = (state = { counter: 0, showCounter: true }, action) => {
 
     switch (action.type) {
         case 'increment':
             return {
                 ...state,
                 counter: state.counter + 1, 
+                showCounter: state.showCounter
+            };
+        case 'increase':
+            return {
+                ...state,
+                counter: state.counter + action.amount,
+                showCounter: state.showCounter
             };
         case 'decrement':
             return {
                 ...state,
                 counter: state.counter - 1,
+                showCounter: state.showCounter
             };
         default:
             return state;
